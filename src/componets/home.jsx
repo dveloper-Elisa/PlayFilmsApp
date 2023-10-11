afa0876dce49764fb8c335e53c0ec923e2ff623e
dveloper-Elisa <kwizeraelisa77@gmail.com>
2023-10-12 00:46:43 +1400

    adding loader to the page

--- src/componets/home.jsx
+++ src/componets/home.jsx
@@ -4,6 +4,7 @@ import Card from "./filmCard";
 
 const Home = () => {
   const [cards, setCards] = useState([]);
+  const [loader, setLoader] = useState(true);
   useEffect(() => {
     const getCards = async () => {
       const url =
@@ -20,6 +21,9 @@ const Home = () => {
 
       try {
         const result = await axios.get(url, options);
+        if (result) {
+          setLoader(false);
+        }
         setCards(result.data.results);
       } catch (err) {
         console.log(err);
@@ -29,43 +33,51 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="grid items-center">
-      <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-3">
-        {cards.slice(0, 7).map((item, index) => {
-          return (
-            <Card
-              key={index}
-              image={item.backdrop_path}
-              name={item.original_title}
-              id={item.id}
-            />
-          );
-        })}
-      </div>
-      <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-3">
-        {cards.slice(8, 15).map((item, index) => {
-          return (
-            <Card
-              key={index}
-              image={item.backdrop_path}
-              name={item.original_title}
-              id={item.id}
-            />
-          );
-        })}
-      </div>
-      <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-3">
-        {cards.slice(13, 20).map((item, index) => {
-          return (
-            <Card
-              key={index}
-              image={item.backdrop_path}
-              name={item.original_title}
-              id={item.id}
-            />
-          );
-        })}
-      </div>
+    <div>
+      {loader ? (
+        <div className={`flex justify-center ${loader ? "flex" : "hidden"}`}>
+          <img src="./loading.gif" alt="loading." />
+        </div>
+      ) : (
+        <div className={`grid items-center ${loader ? "hidden" : "grid"}`}>
+          <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-3">
+            {cards.slice(0, 7).map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  image={item.backdrop_path}
+                  name={item.original_title}
+                  id={item.id}
+                />
+              );
+            })}
+          </div>
+          <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-3">
+            {cards.slice(8, 15).map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  image={item.backdrop_path}
+                  name={item.original_title}
+                  id={item.id}
+                />
+              );
+            })}
+          </div>
+          <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-3">
+            {cards.slice(13, 20).map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  image={item.backdrop_path}
+                  name={item.original_title}
+                  id={item.id}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
